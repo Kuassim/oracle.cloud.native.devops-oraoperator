66d5de6f8325948ad4526255abf972328e32bd77
naberin <aberinnj@gmail.com>
2022-08-09 15:57:08 -0500

    Update init-cluster.sh

--- examples/cloudbank/scripts/init-cluster.sh
+++ examples/cloudbank/scripts/init-cluster.sh
@@ -22,7 +22,4 @@ $CB_STATE_DIR/gen-lb-cert.sh
 # Create secret for frontend-password
 kubectl create secret generic cloudbank-password --from-literal=password=$(state_get .lab.pwd.login)
 
-# Create secret for Schema password
-kubectl create secret generic admin-password --from-literal=admin-password=$(state_get .lab.fixed_demo_user_credential)
-
 state_set '.state.clustersetup.DONE|= $VAL' "$( date '+%F_%H:%M:%S' )"
